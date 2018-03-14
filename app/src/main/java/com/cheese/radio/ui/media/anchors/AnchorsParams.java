f27948562c3f6c64174922d7cdbdb99953a73021
jby292837678 <292837678@qq.com>
2018-03-14 20:05:13 +0800

    updata

--- app/src/main/java/com/cheese/radio/ui/media/anchors/AnchorsParams.java
+++ app/src/main/java/com/cheese/radio/ui/media/anchors/AnchorsParams.java
@@ -1,5 +1,6 @@
 package com.cheese.radio.ui.media.anchors;
 
+import com.cheese.radio.base.IkeParams;
 import com.cheese.radio.util.CheeseApiParams;
 import com.cheese.radio.util.MD5;
 import com.cheese.radio.util.MyBaseUtil;
@@ -12,10 +13,9 @@ import java.util.Iterator;
  * Created by 29283 on 2018/3/11.
  */
 
-public class AnchorsParams extends CheeseApiParams{
+public class AnchorsParams extends IkeParams{
     private String uuid;
     private String timestamp;
-    private String sign;
 
     public String getUuid() {
         return uuid= MyBaseUtil.getMacAddress();
@@ -35,48 +35,7 @@ public class AnchorsParams extends CheeseApiParams{
         this.timestamp = String.valueOf(System.currentTimeMillis());
     }
 
-//    public String getSign() {
-////        String head="timestamp"+getTimestamp()+"uuid"+getUuid()+MyBaseUtil.getRandoms(8);
-//        String head="timestamp"+"="+getTimestamp()+MyBaseUtil.getRandoms(8);
-//        String tail= MD5.getInstance().getMD5(head);
-//        return sign=head+tail;
-//    }
 
-    public String getSign(){
-        if(sign!=null) return sign;
-       HashMap<String,String> argParams=new HashMap<>() ;
-        argParams.put("uuid",getUuid());
-        argParams.put("timestamp",getTimestamp());
-        argParams.put("phone","17857025659");
-        String randomString =MyBaseUtil.getRandoms(8);
-        StringBuilder sb=new StringBuilder();
-        int index =0;
-        String[] paramArray=new String[argParams.size()];
-        Iterator<HashMap.Entry<String,String>> iterator=argParams.entrySet().iterator();
-        while (iterator.hasNext()){
-            sb.setLength(0);
-            HashMap.Entry<String,String> en =iterator.next();
-            paramArray[index++] =sb.append(en.getKey()).append("=").append(en.getValue()).toString();
-        }
-
-        Arrays.sort(paramArray);
-        sb.setLength(0);
-        for(int i=0;i<paramArray.length;++i){
-            sb.append(paramArray[i]).append("&");
-        }
-        sb.setLength(sb.length()-1);
-        sb.append(randomString);
-        return sign=randomString+MD5.getInstance().getMD5(sb.toString()).toUpperCase();
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
-
-    @Override
-    public String toString() {
-        return getSign();
-    }
 
 
 
