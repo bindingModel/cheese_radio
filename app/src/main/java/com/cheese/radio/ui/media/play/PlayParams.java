505c8a65dc1d5aaec20bc00abe974071689860e0
arvin <1033144294@qq.com>
2018-03-18 17:07:32 +0800

    blur

--- app/src/main/java/com/cheese/radio/ui/media/play/PlayParams.java
+++ app/src/main/java/com/cheese/radio/ui/media/play/PlayParams.java
@@ -15,6 +15,15 @@ public class PlayParams extends IkeParams {
         this.id = id;
     }
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getMethod() {
         return method;
     }
