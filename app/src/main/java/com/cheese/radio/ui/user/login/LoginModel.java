f0d625fc4cc08101c5e1cbf5525d58b02c5cc668
jby292837678 <292837678@qq.com>
2018-03-19 18:47:57 +0800

    3-19

--- app/src/main/java/com/cheese/radio/ui/user/login/LoginModel.java
+++ app/src/main/java/com/cheese/radio/ui/user/login/LoginModel.java
@@ -25,38 +25,46 @@ import static com.cheese.radio.inject.component.ActivityComponent.Router.home;
  * Created by 29283 on 2018/3/5.
  */
 @ModelView(R.layout.activity_login)
-public class LoginModel extends ViewModel<LoginActivity,ActivityLoginBinding> {
-    @Inject LoginModel(){
+public class LoginModel extends ViewModel<LoginActivity, ActivityLoginBinding> {
+    @Inject
+    LoginModel() {
 
     }
-    private SmsParams SMSparams=new SmsParams("sendValidCode");
-    private SignParams signParams=new SignParams("login");
+
+    private SmsParams SMSparams = new SmsParams("sendValidCode", "login");
+    private SignParams signParams = new SignParams("login");
     @Inject
     RadioApi api;
+
     @Override
     public void attachView(Bundle savedInstanceState, LoginActivity loginActivity) {
         super.attachView(savedInstanceState, loginActivity);
         getDataBinding().setParams(signParams);
     }
 
-    public void onGetSmsClick(View view){
+    public void onGetSmsClick(View view) {
         if (!signParams.isValidPhone((TextView) view)) return;
         api.getSMS(SMSparams.setPhone(signParams.getPhone()))
                 .compose(new ErrorTransform<>())
-                .subscribe(objectInfoEntity ->{
-                    if(0==(objectInfoEntity.getCode()))
-                        BaseUtil.sendSMSSuccess((TextView)view);
-                    else BaseUtil.sendSMSFailed(view,objectInfoEntity.getMessage());
-                } );
+                .subscribe(objectInfoEntity -> {
+                    if (0 == (objectInfoEntity.getCode()))
+                        BaseUtil.sendSMSSuccess((TextView) view);
+                    else BaseUtil.sendSMSFailed(view, objectInfoEntity.getMessage());
+                });
     }
-    public void onSignClick(View view){
+
+    public void onSignClick(View view) {
         if (!signParams.isValidPhone((TextView) view)) return;
         if (!signParams.isValidSMS((TextView) view)) return;
+        signParams.setLoginType("phone");
         api.getToken(signParams).compose(new RestfulTransformer<>())
-                .subscribe(signUserEntity -> IkeApplication.getUser().setToken(signUserEntity.getToken()));
+                .subscribe(signUserEntity -> {
+                    IkeApplication.getUser().setToken(signUserEntity.getToken());
+                }, BaseUtil::toast);
 
     }
-    public void onGoHoneClick(View view){
+
+    public void onGoHoneClick(View view) {
         ARouterUtil.navigation(home);
     }
 }
