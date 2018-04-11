cb5e65bac9633309a8c6ab9622a17e5bb155c5f8
jby292837678 <292837678@qq.com>
2018-04-11 12:48:09 +0800

    4-11

--- app/src/main/java/com/cheese/radio/ui/media/play/PlayEntity.java
+++ app/src/main/java/com/cheese/radio/ui/media/play/PlayEntity.java
@@ -140,8 +140,8 @@ public class PlayEntity implements Entity {
     public CharSequence getMsg() {
 
         return BaseUtil.colorText(
-                T(anchorName, true,"ffffff"),
-                T(anchorBrief, false, "FFFFFF")
+                T(anchorName, true,"111111"),
+                T(anchorBrief, false, "111111")
         );
     }
     public void onClick(View view){
