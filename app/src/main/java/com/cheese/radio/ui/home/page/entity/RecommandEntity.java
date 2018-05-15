84297bd5f80647ba341f6a661e38a034d89eb602
jby292837678 <292837678@qq.com>
2018-05-15 21:57:00 +0800

    5-15

--- app/src/main/java/com/cheese/radio/ui/home/page/entity/RecommandEntity.java
+++ app/src/main/java/com/cheese/radio/ui/home/page/entity/RecommandEntity.java
@@ -6,6 +6,7 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.binding.model.App;
@@ -22,7 +23,7 @@ import com.cheese.radio.databinding.ItemHomePageRecommandBinding;
  * Created by 29283 on 2018/3/5.
  */
 @ModelView(R.layout.item_home_page_recommand)
-public class RecommandEntity extends ViewInflateRecycler <ItemHomePageRecommandBinding> implements SpanSize, GridInflate<ItemHomePageRecommandBinding> {
+public class RecommandEntity extends ViewInflateRecycler<ItemHomePageRecommandBinding> implements SpanSize, GridInflate<ItemHomePageRecommandBinding> {
     /**
      * image : http://cheese-radio-1256030909.cos.ap-guangzhou.myqcloud.com/images/c7/c28/1622424e9c762.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDzLbkmgG9mDR0VpMufGguwldS4VknuIl8%26q-sign-time%3D1521024826%3B1522118831%26q-key-time%3D1521024826%3B1522118831%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D599d8ef7d988ba6db4a40dfd7e9cc0af617498c3
      * playCount : 0
@@ -41,12 +42,27 @@ public class RecommandEntity extends ViewInflateRecycler <ItemHomePageRecommandB
     private int index;
     private int spansize = 2;
 
+
     @Override
     public ItemHomePageRecommandBinding attachView(Context context, ViewGroup co, boolean attachToParent, ItemHomePageRecommandBinding binding) {
         super.attachView(context, co, attachToParent, binding);
-        ImageView imageView =getDataBinding().imageView;
-//        imageView.setLayoutParams();
-        return null;
+        WindowManager wm = (WindowManager) App.getCurrentActivity().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;// 屏幕宽度（像素）
+        int height = dm.heightPixels; // 屏幕高度（像素）
+        float density = dm.density;//屏幕密度（0.75 / 1.0 / 1.5）
+        int densityDpi = dm.densityDpi;//屏幕密度dpi（120 / 160 / 240）
+        //屏幕宽度算法:屏幕宽度（像素）/屏幕密度
+        int screenWidth = (int) (width / density);//屏幕宽度(dp)
+        int screenHeight = (int) (height / density);//屏幕高度(dp)
+        int imageHeght = screenWidth*161 / 120;
+        int imageWidth= screenWidth*3;
+        ImageView imageView = getDataBinding().imageView;
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(-1,  imageHeght);
+        imageView.setLayoutParams(params);
+        return super.attachView(context, co, attachToParent, binding);
+
     }
 
     public String getImage() {
