dfd2663b828a12677a38a9a3101a1b67d28bbec4
jby292837678 <292837678@qq.com>
2018-05-17 16:43:11 +0800

    5-17

--- app/src/main/java/com/cheese/radio/ui/user/service/center/CenterModel.java
+++ app/src/main/java/com/cheese/radio/ui/user/service/center/CenterModel.java
@@ -1,5 +1,15 @@
 package com.cheese.radio.ui.user.service.center;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.binding.model.App;
 import com.binding.model.model.ModelView;
 import com.binding.model.model.ViewHttpModel;
 import com.cheese.radio.R;
@@ -13,12 +23,33 @@ import javax.inject.Inject;
 @ModelView(R.layout.activity_service_center)
 public class CenterModel extends ViewHttpModel<CenterActivity, ActivityServiceCenterBinding, Object> {
 
-    @Inject CenterModel(){}
-
+    @Inject
+    CenterModel() {
+    }
 
 
     @Override
     public void accept(Object o) throws Exception {
 
+    }
+
+    @Override
+    public void attachView(Bundle savedInstanceState, CenterActivity centerActivity) {
+        super.attachView(savedInstanceState, centerActivity);
+        ImageView imageView = getDataBinding().adImage;
+        WindowManager wm = (WindowManager) App.getCurrentActivity().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;// 屏幕宽度（像素）
+        int height = dm.heightPixels; // 屏幕高度（像素）
+        float density = dm.density;//屏幕密度（0.75 / 1.0 / 1.5）
+        int densityDpi = dm.densityDpi;//屏幕密度dpi（120 / 160 / 240）
+        //屏幕宽度算法:屏幕宽度（像素）/屏幕密度
+        int screenWidth = (int) (width / density);//屏幕宽度(dp)
+        int screenHeight = (int) (height / density);//屏幕高度(dp)
+        int imageWidth = (int) (width);
+        int imageHeght = (int) imageWidth * 2 / 3;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(imageWidth, imageHeght);
+        imageView.setLayoutParams(params);
     }
 }
