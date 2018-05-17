33e5d7540cf46774105d804e5d6426bc6b1d82b4
jby292837678 <292837678@qq.com>
2018-05-17 09:45:54 +0800

    5-17

--- app/src/main/java/com/cheese/radio/ui/media/anchor/entity/description/DescriptionFragmentModel.java
+++ app/src/main/java/com/cheese/radio/ui/media/anchor/entity/description/DescriptionFragmentModel.java
@@ -13,6 +13,7 @@ import com.cheese.radio.R;
 import com.cheese.radio.databinding.FragmentAnchorMsgBinding;
 import com.cheese.radio.ui.Constant;
 import com.cheese.radio.ui.media.group.fragment.introduce.GroupIntroduceFragment;
+import com.cheese.radio.util.MyBaseUtil;
 
 import javax.inject.Inject;
 
@@ -33,39 +34,7 @@ public class DescriptionFragmentModel extends ViewModel<DescriptionFragment,Frag
             descriptionText.set(bundle.getString(Constant.description));
         }
         webView = getDataBinding().webview;
-//        webView.loadUrl("file:///android_asset/test.html");//加载asset文件夹下html
-        //   webView.loadUrl("http://139.196.35.30:8080/OkHttpTest/apppackage/test.html");//加载url
-
-        //       使用webview显示html代码
-        webView.loadDataWithBaseURL(null,descriptionText.get(), "text/html" , "utf-8", null);
-
-        //  webView.addJavascriptInterface(this,"android");//添加js监听 这样html就能调用客户端
-//        webView.setWebChromeClient(webChromeClient);
-//        webView.setWebViewClient(webViewClient);
-
-        WebSettings webSettings=webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);//允许使用js
-        webSettings.setTextZoom(100);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-//        webSettings.setTextSize(WebSettings.TextSize.SMALLER);
-        /**
-         * LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
-         * LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
-         * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
-         * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
-         */
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//不使用缓存，只从网络获取数据.
-
-        //支持屏幕缩放
-//        webSettings.setSupportZoom(true);
-//        webSettings.setBuiltInZoomControls(true);
-//        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webSettings.setLoadWithOverviewMode(true);
-        //不显示webview缩放按钮
-        webSettings.setDisplayZoomControls(false);
+        MyBaseUtil.setWebView(webView,descriptionText.get());
     }
 
 }
--- app/src/main/java/com/cheese/radio/util/MyBaseUtil.java
+++ app/src/main/java/com/cheese/radio/util/MyBaseUtil.java
@@ -289,5 +289,41 @@ public class MyBaseUtil extends BaseUtil {
                 bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
     }
+    public static  WebView setWebView(WebView webView,String text){
+//        webView.loadUrl("file:///android_asset/test.html");//加载asset文件夹下html
+        //   webView.loadUrl("http://139.196.35.30:8080/OkHttpTest/apppackage/test.html");//加载url
+
+        //       使用webview显示html代码
+        webView.loadDataWithBaseURL(null,text, "text/html" , "utf-8", null);
+
+        //  webView.addJavascriptInterface(this,"android");//添加js监听 这样html就能调用客户端
+//        webView.setWebChromeClient(webChromeClient);
+//        webView.setWebViewClient(webViewClient);
+
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);//允许使用js
+        webSettings.setTextZoom(100);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+//        webSettings.setTextSize(WebSettings.TextSize.SMALLER);
+        /**
+         * LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
+         * LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
+         * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
+         * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
+         */
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//不使用缓存，只从网络获取数据.
+
+        //支持屏幕缩放
+//        webSettings.setSupportZoom(true);
+//        webSettings.setBuiltInZoomControls(true);
+//        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setLoadWithOverviewMode(true);
+        //不显示webview缩放按钮
+        webSettings.setDisplayZoomControls(false);
+        return webView;
+    }
 }
 
